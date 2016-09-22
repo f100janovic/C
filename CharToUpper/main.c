4812f09b253b76e5e0d9b225649ba00e905bb8da
nbadnjarevic <nbadnjarevic@hotmail.com>
2016-09-23 00:34:52 +0200

    Added a project which changes the char from lowercase to uppercase and vice versa

---
+++ CharToUpper/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Changes the char from lowercase to uppercase
+and vice versa
+*/
+int main()
+{
+	char a;
+	scanf("%c", &a);
+	if (a>=65 && a<=90){
+		a=a+32;
+	}else if (a>=90 && a<=123){
+		a=a-32;
+	}
+	printf("%c\n", a);
+	return 0;
+}
